316d68b4e003b8c94eeb2d88845318b79d262ced
Ray Foss <ray@vblip.com>
2017-07-28 15:09:22 -0500

    ... you can literally just remove it and nothing break. single souce of truth is still broken.

--- src/api/feathers-client.js
+++ src/api/feathers-client.js
@@ -6,8 +6,6 @@ import auth from 'feathers-authentication-client'
 import io from 'socket.io-client'
 import feathersVuex from 'feathers-vuex'
 import store from '@/store/'
-import rx from 'feathers-reactive'
-import RxJS from 'rxjs'
 
 const socket = io('http://localhost:3030', {transports: ['websocket']})
 
@@ -15,7 +13,6 @@ const feathersClient = feathers()
   .configure(hooks())
   .configure(socketio(socket))
   .configure(auth({ storage: window.localStorage }))
-  .configure(rx(RxJS, {idField: '_id'}))
   .configure(feathersVuex(store, {
     idField: '_id',
     auth: {
